b5c883d3481102869c022a2d17bd12f74c901592
LeN0UR <aann20034@gmail.com>
2025-12-09 20:45:22 +0000

    tffmt backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
-    bucket         = "memos-terraform-state-nourdemo"  
+    bucket         = "memos-terraform-state-nourdemo"
     key            = "memos/terraform.tfstate"
     region         = "eu-west-2"
     encrypt        = true
-    dynamodb_table = "memos-terraform-locks" 
+    dynamodb_table = "memos-terraform-locks"
   }
 }
 
